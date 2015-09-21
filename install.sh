8457c8b2d6e1553a081691a1a4dfb725e956842e
acutesoftware <djmurray@acutesoftware.com.au>
2015-09-21 19:32:20 +0930

    initial versions of scripts

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+########################################################
+# install.sh
+# Script to run on new VM to backup dotfiles
+# and copy standard ones to home
+########################################################
+
+master=~/dev/dotfiles           # master dotfiles directory
+olddir=~/dev/dotfiles_old       # old dotfiles backup directory
+
+# list of files/folders to copy
+files=".bashrc .gitconfig p"                 
+
+# list of packages to install
+packages_to_install="python-pip git xchat wine1.7"
+
+if [ -d $olddir ]; 
+then
+	# backup directory has already been created by this script so
+	# install latest dotfiles by copying from the master /dev/dotfiles
+	# folder to home (along with useful scripts)
+	echo "Installing latest scripts and dot files from $master to ~ "
+	for file in $files; 
+	do
+		cp $master/$file ~/$file
+
+	done
+	
+else
+	# backup directory doesnt exist yet, so this is a fresh install
+	#create backup folder and copy and existing files there 
+	
+	echo "Creating $olddir for backup of any existing dotfiles in ~"
+	mkdir -p $olddir
+
+	for file in $files; 
+	do
+		echo "Backing up $file from ~ to $olddir"
+		cp ~/$file $olddir/$file.orig
+
+	done
+	
+	# now run apt-get to install standard packages
+	echo "About to install the packages : $packages_to_install"
+	for package in $packages_to_install;
+	do
+		echo "Installing pacakge $package"
+		sudo apt-get install $package
+	done
+fi
---
+++ p
@@ -0,0 +1,17 @@
+#!/bin/bash
+########################################################
+# p
+# Project script to handle moving to folders and setting
+# up new git projects
+########################################################
+
+projects="python/AIKIF python/ext-dl python/filelist python/gfx python/lifepim python/LifePIM_public python/rawdata python/tax python/virtual_AI_simulator python/worldbuild"
+echo "TODO = use command param to switch to folder based on first letters. You are in folder "
+pwd
+
+for proj in $projects;
+do
+	echo "Project : $proj"
+	
+	# todo 
+done;
