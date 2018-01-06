aafc046de28a940357e4378c9b48db704fd28c25
Duncan Murray <djmurray@gmail.com>
2018-01-06 21:30:25 +1030

    notes on setting up SSL on pythonanywhere

--- setup_acute_linux.sh
+++ setup_acute_linux.sh
@@ -41,6 +41,31 @@ do
 
 done;
 
+# Setup files needed to provide SSL for domains ( www.lifepim.com )
+# NOTE - dont run this locally, run this in a BASH shell on pythonanywhere
+git config --global core.autocrlf false
+git clone https://github.com/lukas2511/dehydrated.git ~/dehydrated
+mkdir -p ~/letsencrypt/wellknown
+cd ~/letsencrypt
+
+# STOP - manually setup mappings on pythonanywhere.com
+#  You'll also need your pythonanywhere site to be able to serve static files from your wellknown directory. 
+# Head over to web app tab and set up a new mapping:
+#  Static URL: /.well-known/acme-challenge
+#  Static Path: /home/acutesoftware/letsencrypt/wellknown
+
+vim /home/acutesoftware/letsencrypt/config
+:i WELLKNOWN=/home/acutesoftware/letsencrypt/wellknown
+
+~/dehydrated/dehydrated --config ~/letsencrypt/config --cron --domain www.lifepim.com --out ~/letsencrypt --challenge http-01
+
+# then email support to get the certificate installed
+
+# DONE
+
+# Every 3 months - you need to renew the certificate as follows
+cd ~/letsencrypt
+~/dehydrated/dehydrated --cron --domain www.yourdomain.com --out . --challenge http-01
 
 
 
